dbb1c43d9371244ca2e6df313c4a2209b8e10ca5
Amery_J <820293142@qq.com>
2020-01-03 18:11:35 +0800

    接近成功

--- 8.py
+++ 8.py
@@ -2,11 +2,17 @@ import pyautogui
 import time
 import random
 
-screenWidth, screenHeight = pyautogui.size()
-pyautogui.moveTo(screenWidth / 2, screenHeight / 2)
-# # pyautogui.PAUSE = 2.5
-# # pyautogui.moveTo(100, 100)
-# # pyautogui.click()
+# screenWidth, screenHeight = pyautogui.size()  # 获得当前屏幕的宽高
+
+# pyautogui.moveTo(screenWidth / 2, screenHeight / 2)  # 移动光标至屏幕中心
+#
+# pyautogui.PAUSE = 2.5  # 代码执行完毕，延迟2.5秒结束程序
+#
+# pyautogui.moveTo(screenWidth / 4, screenHeight / 4)  # 移动光标至屏幕中心
+
+# pyautogui.moveTo(100, 100)
+
+# pyautogui.click()
 
 # a = pyautogui.position()  # current mouse x and y
 # (968, 56)
@@ -15,29 +21,25 @@ pyautogui.moveTo(screenWidth / 2, screenHeight / 2)
 
 # b = pyautogui.size()
 # print(b)
-#
-#
-#
-# c = pyautogui.onScreen(1500, 910)
+
+# c = pyautogui.onScreen(1500, 910)  # 判断坐标是否在屏幕上，返回值是True或者False
 # print(c)
-# pyautogui.FAILSAFE = True
-# # time.sleep(2)
-# pyautogui.moveTo(300, 300, duration=random.random())
-# pyautogui.moveRel(400, 200, duration=random.random())  # 将鼠标移动到当前位置 move mouse relative to its current position
-# pyautogui.click(button='left')
 
-# pyautogui.typewrite(['a', 'b', 'c', 'left', 'backspace', 'enter', 'f1'], interval=random.random())
-# pyautogui.alert('This displays some text with an OK button.')
-# pyautogui.confirm('This displays text and has an OK and Cancel button.')
-# pyautogui.prompt('This lets the user type in a string and press OK.')
+# pyautogui.moveTo(300, 300, duration=random.random())  # 移动鼠标到目的地
+# pyautogui.moveRel(400, 200, duration=random.random())  # 将鼠标从某点移动到当前位置 move mouse relative to its current position
+# pyautogui.click(button='left')  # 鼠标点击事件，左击
+
+# pyautogui.typewrite(['a', 'b', 'c', 'left', 'backspace', 'enter', 'f1'], interval=random.random())  # 组合键输入，按键间隔随机时间
+# pyautogui.alert('This displays some text with an OK button.')  # OK弹出框
+# pyautogui.confirm('This displays text and has an OK and Cancel button.')  # OK or Cancel 选择弹出框
+# pyautogui.prompt('This lets the user type in a string and press OK.')  # OK or cancel 输入框
 
 
-# d = pyautogui.screenshot()
-# e = pyautogui.screenshot('foo.png')
-# # pyautogui.locateOnScreen()
-# pyautogui.move(0, 50,)
-# pyautogui.drag(100, 200, button='left')
-# pyautogui.dragTo(-100, 200, button='left')
+# d = pyautogui.screenshot()  # 截屏
+# e = pyautogui.screenshot('foo.png')  # 截屏并保存
+pyautogui.move(0, 50,)  # 区别与moveTo，参考点为鼠标当前位置
+# pyautogui.drag(300, 200, button='left')  # 拖拽移动，待定
+# pyautogui.dragTo(300, 200, button='left')  # 拖拽移动至，待定
 
 # pyautogui.keyDown('shift') # hold down the shift key
 # pyautogui.press('a')     # press the left arrow key
@@ -47,20 +49,14 @@ pyautogui.moveTo(screenWidth / 2, screenHeight / 2)
 
 
 # e = pyautogui.screenshot('/Users/yi/Desktop/FORFND/qqq111.png')
-# f = pyautogui.locateOnScreen("/Users/yi/Desktop/FORFND/qqq.png", confidence=0.98)
+# f = pyautogui.locateOnScreen("/Users/yi/Desktop/FORFND/qqq.png", confidence=0.98)  # 图像匹配，返回值为True或者False
 # print(f)
-#
-#
-# pyautogui.pixelMatchesColor(100, 200, (130, 135, 144))
 
 
-im = pyautogui.screenshot()
-print(im)
-print(im.getpixel((100, 200)))
-print()
-pix = pyautogui.pixel(100, 200)
-print(pix)
-i = pyautogui.pixelMatchesColor(100, 200, (143, 134, 168), tolerance=5)
-print(i)
+# im = pyautogui.screenshot()
+# print(im.getpixel((100, 200)))  # 获取某点像素 (13, 41, 62, 255)
+# pix = pyautogui.pixel(100, 200)  # RGB(red=13, green=41, blue=62)
+# print(pix)
 
-pyautogui.moveTo(100, 200)
+# i = pyautogui.pixelMatchesColor(100, 200, (143, 134, 168), tolerance=5)  # 像素匹配，返回值为True或者False
+# print(i)
